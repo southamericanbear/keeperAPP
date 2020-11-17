6c9d3f5307b634181824699999d090de6f5cff04
alan Dittler <dittler.a@gmail.com>
2020-11-17 17:59:16 -0300

    update

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import notes from "../note";
 import Note from "./note";
 
-function App() {
+const App = () => {
   return (
     <div>
       <Header />
       <Footer />
-      <Note />
+      {notes.map((lanota) => (
+        <Note key={lanota.key} title={lanota.title} content={lanota.content} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
