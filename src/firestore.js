1b927d98e3afa5afdf901120dc9468b028d3f3df
alan Dittler <dittler.a@gmail.com>
2020-11-20 14:21:00 -0300

    start firestore

---
+++ src/firestore.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAKCvo3Dp7Mu1fEjtEI7mkWN1Y_ixSCa2M",
+  authDomain: "keeperapp-70f03.firebaseapp.com",
+  databaseURL: "https://keeperapp-70f03.firebaseio.com",
+  projectId: "keeperapp-70f03",
+  storageBucket: "keeperapp-70f03.appspot.com",
+  messagingSenderId: "801993134305",
+  appId: "1:801993134305:web:74d32e94f83e7628efe7a0",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
